7c4268740fcebe471e4cdf75d54d01f87c5bcedd
Rodrigo Cabrera <rocabrera1994@gmail.com>
2023-03-09 19:48:13 -0300

    agregar imagenes en card

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -5,7 +5,9 @@ const PokemonCard = ({pokemon}) => {
   return (
   
     <div className='pokemon-container'>
-      <img src={pokemon.sprites.other.dream_world.front_default} className='pokemon-img' />
+      <div className="circle">
+        <img src={pokemon.sprites.other.dream_world.front_default} className='pokemon-img' />
+      </div>
       <p className='pokemon-name'>{pokemon.name}</p>
       <p className='pokemon-id'>id: {pokemon.id}</p>
       <p className='pokemon-type'>type: {pokemon.types[0].type.name}</p>
